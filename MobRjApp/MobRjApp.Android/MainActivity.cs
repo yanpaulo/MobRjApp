b9a9c2cb1163ebcaca8b74dd250c45833097113a
Yan Paulo <yanpaulo@hotmail.com>
2019-05-02 12:24:24 -0300

    Launcher icon

--- MobRjApp/MobRjApp.Android/MainActivity.cs
+++ MobRjApp/MobRjApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MobRjApp.Droid
 {
-    [Activity(Label = "MobRjApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MOBRJ", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
